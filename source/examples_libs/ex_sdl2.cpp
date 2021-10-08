b774b9194d7cacd2f52088a4a42eb151c77823e0
Diego Hurtado <diegohurtado@ug.uchile.cl>
2021-10-08 12:21:34 -0300

    Retried couple times to correctly import SDL

--- source/examples_libs/ex_sdl2.cpp
+++ source/examples_libs/ex_sdl2.cpp
@@ -1,22 +1,26 @@
 #include "SDL.h"
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
-
+    SDL_Init(SDL_INIT_EVERYTHING);
+    
     SDL_Window* window = SDL_CreateWindow(
         "SDL2Test",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
+        30,
+        30,
         640,
         480,
-        0
+        SDL_WINDOW_SHOWN
     );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
+
+    SDL_Surface* screen = SDL_GetWindowSurface(window);
+
 
     SDL_Delay(3000);
 
