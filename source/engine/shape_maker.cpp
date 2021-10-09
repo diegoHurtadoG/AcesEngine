634d59e82aecff9aaf641e60ec94298b0a5af2dd
Diego Hurtado <diegohurtado@ug.uchile.cl>
2021-10-09 16:49:49 -0300

    Made a board and a card as an example

--- source/engine/shape_maker.cpp
+++ source/engine/shape_maker.cpp
@@ -12,9 +12,37 @@
 // This function is to test the art i will be doing in the functions
 
 int main() {
+
+    // Initialize a Render Window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Close | sf::Style::Resize);
-    sf::RectangleShape card(sf::Vector2f(50.0f, 80.0f));
-    card.setFillColor(sf::Color::Red);
+
+    // Make a rectangle to put on background
+    sf::Texture background_texture;
+    if (!background_texture.loadFromFile("C:/Users/diego/OneDrive/Desktop/Ramos/2021-2/Arquitectura de Motores de Videojuegos/Proyecto/AcesEngine/assets/imgs/backgrounds/background_circles.jpg"))
+    {
+        printf("Error loading background texture");
+        return -1;
+    }
+    sf::RectangleShape rectangle_test(sf::Vector2f(800.0f, 600.0f));
+    rectangle_test.setTexture(&background_texture);
+
+
+    // Initialize a texture of a card
+    sf::Texture texture_test;
+    // Ath (1, 1) of the .png, loads a 32x44 image
+    if (!texture_test.loadFromFile("C:/Users/diego/OneDrive/Desktop/Ramos/2021-2/Arquitectura de Motores de Videojuegos/Proyecto/AcesEngine/assets/imgs/8BitDeckAssets.png", sf::IntRect(1, 1, 32, 44)))
+    {
+        printf("Error loading texture");
+        return -1;
+    }
+    texture_test.setSmooth(true);
+
+
+    // Initialize a Sprite to stick the texture in
+    sf::Sprite sprite_test;
+    sprite_test.setTexture(texture_test);
+    sprite_test.setPosition(sf::Vector2f(33.0f, 0.0f));
+
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -39,7 +67,8 @@ int main() {
 
         }
 
-        window.draw(card);
+        window.draw(rectangle_test);
+        window.draw(sprite_test);
         window.display();
 
     }
